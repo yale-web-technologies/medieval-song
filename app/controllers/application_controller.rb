84d13be99d7ae6673ff17d64cd32c904c728deac
Seong-June Kim <seong-june.kim@yale.edu>
2018-01-03 10:09:36 -0500

    Redirect to home page when not authorized for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception, prepend: true
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def login
     authenticate_user!
@@ -10,5 +11,12 @@ class ApplicationController < ActionController::Base
   # Devise: run after successful sign-in.
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized for administrative functions."
+    redirect_to(request.referrer || root_path)
   end
 end
